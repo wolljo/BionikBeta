767dd35ec9e317f92ae19f9c5d6dea0daab04cd0
wolljo <wolljo@gmail.com>
2025-09-02 14:34:08 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// Sehr minimalistischer SW für spätere PWA-Erweiterung (aktuell: passthrough).
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', () => {}); // kein aggressives Caching in v0.2
